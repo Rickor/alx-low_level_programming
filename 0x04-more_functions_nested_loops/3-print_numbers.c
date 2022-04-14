6cc3b5d36937b2f408b5bee8ce2f78a455e9912b
Rickor <derrickodago13@gmail.com>
2022-04-14 14:44:44 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_numbers - entry point
+*
+*Return: always 0
+*/
+void print_numbers(void)
+{
+
+int n;
+for (n = 0; n < 10; n++)
+{
+_putchar(n);
+}
+
+_putchar('\n');
+
+return (0);
+}
