9df1a16f852cdb2d91ebb01a2d345e2cfe8cdd9a
Rickor <derrickodago13@gmail.com>
2022-04-14 15:18:04 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_line - print
+*@n - print int
+*@n: print int
+*Return: always 0
+*/
+void print_line(int n)
+{
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar (95);
+}
+
+_putchar('\n');
+}
+
+else
+{
+_putchar('\n');
+}
+
