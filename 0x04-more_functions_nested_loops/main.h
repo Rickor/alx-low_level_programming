4d64f7769a4e39cfc17408d5eba6231e0ce1046c
Rickor <derrickodago13@gmail.com>
2022-04-14 16:50:04 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,24 +1,10 @@
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
-void print_line(int n);
-void more_numbers(void);
-void print_most_numbers(void);
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
 int _isupper(int c);
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
