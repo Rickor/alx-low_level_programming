6b6565c53cc7f68104cb85e6f64b79e77a47ee73
Rickor <derrickodago13@gmail.com>
2022-04-20 01:18:42 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen - length of string
+*@s: pointer to string
+*Return: always 0
+*/
+int _strlen(char *s)
+{
+int f;
+f = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
