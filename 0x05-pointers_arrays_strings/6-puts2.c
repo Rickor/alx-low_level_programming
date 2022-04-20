82d3fceed1ea6f3edbd9cd94a4969b09f0812c4d
Rickor <derrickodago13@gmail.com>
2022-04-20 02:20:01 -0700

    Task 6 update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,16 +4,16 @@
 *@str: pointer to string
 *Return: always 0
 */
-void puts2(char *str)
+void puts2(char *s)
 {
 int a;
+
 for (a = 0; str[a] != '\0'; a++)
 {
 if (a % 2 == 0)
 {
 _putchar (str[a]);
 }
-
-_putchar('\n');
 }
+_putchar('\n');
 }
